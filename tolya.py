ead1f85304d531fe9ed83fff78d9b838b92fd0aa
Anatoly Nowickov <149452476+Anatolypy@users.noreply.github.com>
2024-04-21 17:59:19 +0900

    Почти готово

--- tolya.py
+++ tolya.py
@@ -1,35 +1,51 @@
 import telebot
 import sqlite3
 
-photo1 = "https://thumbs.dreamstime.com/b/egg-roll-spring-roll-popiah-vietnamese-cuisine-thai-style-sweet-suace-wooden-table-71117155.jpg"
-
 bot = telebot.TeleBot('6979060436:AAF9J29cBCUadUi5dp2Dq7g1HUzr1CzdKtc')
 file = open('phrases.txt', encoding='utf-8')
 f = file.read()
 newf = f.split("\n")
 
+ans1 = None
+ans2 = None
+ans3 = None
+ans4 = None
+ans5 = None
+
 
 @bot.message_handler(commands=['start'])
 def main(message):
+    conn = sqlite3.connect('tolya.sql')
+    cur = conn.cursor()
+    cur.execute(
+        '''CREATE TABLE IF NOT EXISTS users (id auto_increment primary key, name varchar(50), name_id varchar(50))''')
+    conn.commit()
+    cur.close()
+    conn.close()
     bot.send_message(message.chat.id, f'Привет {message.from_user.first_name} {message.from_user.last_name}')
     bot.send_message(message.chat.id, newf[0])
     bot.register_next_step_handler(message, test1)
 
 
 @bot.message_handler(content_types=['text'])
-def test1(message):
+def main(message):
     if message.text.lower() == 'нет':
         bot.send_message(message.chat.id, 'Ну хорошо, до скорого')
     else:
-        btn1 = telebot.types.KeyboardButton('Мясо')
-        btn2 = telebot.types.KeyboardButton('Рыба')
-        btn3 = telebot.types.KeyboardButton('Морепродукты')
-        btn4 = telebot.types.KeyboardButton('Другое')
-        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.add(btn1, btn2)
-        markup.add(btn3, btn4)
-        bot.send_photo(message.chat.id, photo="https://i.artfile.ru/3000x2000_811359_%5Bwww.ArtFile.ru%5D.jpg", caption=newf[2], reply_markup=markup)
-        bot.register_next_step_handler(message, test2)
+        bot.register_next_step_handler(message, test1)
+
+
+def test1(message):
+    btn1 = telebot.types.KeyboardButton('Мясо')
+    btn2 = telebot.types.KeyboardButton('Рыба')
+    btn3 = telebot.types.KeyboardButton('Морепродукты')
+    btn4 = telebot.types.KeyboardButton('Другое')
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(btn1, btn2)
+    markup.add(btn3, btn4)
+    bot.send_photo(message.chat.id, photo="https://i.artfile.ru/3000x2000_811359_%5Bwww.ArtFile.ru%5D.jpg",
+                   caption=newf[2], reply_markup=markup)
+    bot.register_next_step_handler(message, test2)
 
 
 def test2(message):
@@ -40,7 +56,9 @@ def test2(message):
     markup.add(btn1)
     markup.add(btn2)
     markup.add(btn3)
-    bot.send_photo(message.chat.id, photo="https://thenewsgod.com/wp-content/uploads/2021/08/load-image-2021-08-01T115912.108.jpeg", caption=newf[3], reply_markup=markup)
+    bot.send_photo(message.chat.id,
+                   photo="https://thenewsgod.com/wp-content/uploads/2021/08/load-image-2021-08-01T115912.108.jpeg",
+                   caption=newf[3], reply_markup=markup)
     bot.register_next_step_handler(message, test3)
 
 
@@ -52,7 +70,8 @@ def test3(message):
     markup.add(btn1)
     markup.add(btn2)
     markup.add(btn3)
-    bot.send_photo(message.chat.id, photo="https://a-z-animals.com/media/2022/05/Best-dog-allergy-tests-header.jpg", caption=newf[4], reply_markup=markup)
+    bot.send_photo(message.chat.id, photo="https://a-z-animals.com/media/2022/05/Best-dog-allergy-tests-header.jpg",
+                   caption=newf[4], reply_markup=markup)
     bot.register_next_step_handler(message, test4)
 
 
@@ -64,7 +83,9 @@ def test4(message):
     markup.add(btn1)
     markup.add(btn2)
     markup.add(btn3)
-    bot.send_photo(message.chat.id, photo="https://ratatum.com/wp-content/uploads/2017/08/1429737953_smart-kezhual-2.jpg", caption=newf[5], reply_markup=markup)
+    bot.send_photo(message.chat.id,
+                   photo="https://ratatum.com/wp-content/uploads/2017/08/1429737953_smart-kezhual-2.jpg",
+                   caption=newf[5], reply_markup=markup)
     bot.register_next_step_handler(message, test5)
 
 
@@ -76,12 +97,26 @@ def test5(message):
     markup.add(btn1)
     markup.add(btn2)
     markup.add(btn3)
-    bot.send_photo(message.chat.id, photo="https://www.factroom.ru/wp-content/uploads/2015/04/Depositphotos_30671459_m.jpg", caption=newf[6], reply_markup=markup)
-    bot.register_next_step_handler(message, summarize)
+    bot.send_photo(message.chat.id,
+                   photo="https://www.factroom.ru/wp-content/uploads/2015/04/Depositphotos_30671459_m.jpg",
+                   caption=newf[6], reply_markup=markup)
+    bot.register_next_step_handler(message, end)
 
 
-def summarize():
-    pass
+def end(message):
+    conn = sqlite3.connect('tolya.sql')
+    cur = conn.cursor()
+    cur.execute(
+        f'''INSERT INTO users (name, name_id) VALUES ('{message.from_user.first_name}', '{message.from_user.id}')''')
+    conn.commit()
+    cur.execute('''SELECT * FROM users''')
+    users = cur.fetchall()
+    info = f'Кто уже прошел тест:\n'
+    for el in users:
+        info += f'Name: {el[1]} ID: {el[2]}\n'
+    cur.close()
+    conn.close()
+    bot.send_message(message.chat.id, info)
 
 
 @bot.message_handler(commands=['help'])
