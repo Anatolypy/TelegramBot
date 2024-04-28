1807ba3ea5d639643d71373724b9917910421806
Anatoly Nowickov <149452476+Anatolypy@users.noreply.github.com>
2024-04-28 16:17:46 +0900

    Update answers.py

--- answers.py
+++ answers.py
@@ -1,29 +1,47 @@
-question1 ={
-    "Мясо": 100,
-    "Рыба": 90,
-    "Морепродукты": 150,
-    "Другое": 50
-
+question1 = {
+    "Мясо": 90,
+    "Рыба": 60,
+    "Морепродукты": 30,
 }
 question2 = {
-    "Постоянно": 100,
-    "Не люблю": 90,
-    "Когда время есть": 150
+    "Постоянно": 90,
+    "Не люблю": 30,
+    "Когда время есть": 60
 }
 question3 = {
-    "Кошки": 100,
+    "Кошки": 60,
     "Собаки": 90,
-    "Другое": 50
+    "Другое": 30
 
 }
 question4 = {
-    "Костюм": 100,
+    "Костюм": 60,
     "Джинсовая куртка": 90,
-    "Другое": 50
+    "Другое": 30
 
 }
 question5 = {
-    "Читаю": 100,
-    "Играю в компьютерные игры": 90,
-    "Гуляю": 150
+    "Читаю": 60,
+    "Играю в компьютерные игры": 30,
+    "Гуляю": 90
+}
+group1 = ["Джон Уик", "Патрик Бэйтман", "Уолтер Уайт"]
+group2 = ["Тайлер Дерден", "Томас Шелби", "K"]
+group3 = ["Гигачад", "Хоумлендар", "Густаво"]
+
+sigmas = {
+    1: {"Джон Уик": "https://www.newsledge.com/wp-content/uploads/2022/10/keanu-reeves-upcoming-movies.jpg",
+        "Патрик Бэйтман": "https://avatars.yandex.net/get-music-content/8123381"
+                          "/4fa1a2b7.a.24998666-1/m1000x1000?webp=false",
+        "Уолтер Уайт": "https://cdn.profile.ru/wp-content/uploads/2019/08/Vo-vse-tyazhkie.jpg"},
+    2: {"Тайлер Дерден": "https://i.ytimg.com/vi/qSlWxveCVDM/maxresdefault.jpg",
+        "Томас Шелби": "https://www.thesun.co.uk/wp-content/uploads/2021/05/NINTCHDBPICT000518866983.jpg",
+        "K": "https://storage.myseldon.com/news-pict-ed/ED10A74D183C77C38760105269431731"},
+    3: {
+        "Гигачад": "https://sun9-43.userapi.com/impg/1CtoFUQ2zUhISUc2nUKA5UFQRYlKE5GCqUpARg/"
+                   "u7q97a6TzD4.jpg?size=1119x823&quality=96&sign=9f20d1894d996847ba3a93583fbf3efb&c_uniq_tag=5iyJ2On"
+                   "Ofkz2DCG_ZP0taBftFwgqJ1XlZHe2YKf6AKk&type=album",
+        "Хоумлендар": "https://img.goodfon.ru/original/6135x4061/f/db/the-boys-amazon-serial"
+                      "-homelander-anthony-starr-superhero.jpg",
+        "Густаво": "https://cdn.dribbble.com/users/1581974/screenshots/4490057/attachments/1017817/originalpic.jpg"}
 }
